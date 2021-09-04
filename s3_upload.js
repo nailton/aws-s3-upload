97aa86832c85f3ac6f955afa50895dddfcd9bc6a
Nailton Sousa <nailton.osn@gmail.com>
2021-09-04 13:05:10 -0300

    clear code

--- s3_upload.js
+++ s3_upload.js
@@ -10,7 +10,7 @@
  * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
  * CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
-*/
+ */
 
 //snippet-sourcedescription:[s3_upload.js demonstrates how to upload an arbitrarily-sized stream to an Amazon S3 bucket.]
 //snippet-service:[s3]
@@ -30,7 +30,8 @@
 require('dotenv').config()
 
 // Load the AWS SDK for Node.js
-var AWS = require('aws-sdk');
+let AWS = require('aws-sdk');
+
 // Set the region 
 AWS.config.update({region: 'sa-east-1'});
 
@@ -38,41 +39,31 @@ AWS.config.update({region: 'sa-east-1'});
 s3 = new AWS.S3({apiVersion: '2006-03-01'});
 
 // call S3 to retrieve upload file to specified bucket
-// chamar S3 para recuperar o arquivo de upload para o BUCKET especificado
+let uploadParams = {Bucket: process.env.AWS_BUCKET + '/set-your-path', Key: '', Body: ''};
 
-// console.log('url bucket...')
-//   console.log(process.env.AWS_BUCKET + '/backup-servidor-antigo/')
-// console.log('fechando url bucket...')
-// return;
-
-var uploadParams = {Bucket: process.env.AWS_BUCKET + '/backup-servidor-antigo', Key: '', Body: ''};
-// Nome, URL do arquivo p/ upload
-// var file = 's3-backup/PASTA_TEST.zip';
-var file = process.argv[2];
+// Name of file to upload
+let file = process.argv[2];
 
 // Configure the file stream and obtain the upload parameters
-// Configure o fluxo de arquivos e obtenha os parâmetros de upload
-var fs = require('fs');
-var fileStream = fs.createReadStream(file);
-fileStream.on('error', function(err) {
-  console.log('File Error', err);
+let fs = require('fs');
+let fileStream = fs.createReadStream(file);
+fileStream.on('error', function (err) {
+    console.log('File Error', err);
 });
 
 uploadParams.Body = fileStream;
-var path = require('path');
+let path = require('path');
 uploadParams.Key = path.basename(file);
 
-// console.log('fileStream...')
-// console.log(uploadParams)
-// return
 
 // call S3 to retrieve upload file to specified bucket
-// chamar S3 para recuperar o arquivo de upload para o bucket especificado
-s3.upload (uploadParams, function (err, data) {
-  if (err) {
-    console.log("Error", err);
-  } if (data) {
-    console.log("Upload Success", data.Location);
-  }
+s3.upload(uploadParams, function (err, data) {
+    if (err) {
+        console.log("Error", err);
+    }
+    if (data) {
+        console.log("Upload Success", data.Location);
+    }
 });
+
 // snippet-end:[s3.JavaScript.buckets.upload]
